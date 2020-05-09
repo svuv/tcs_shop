625b250bd574ff48352b7da0b10074a5e41b81cb
svuv <1737748525@qq.com>
2020-05-10 01:16:54 +0800

    权限功能完成

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -2,7 +2,7 @@
   <div>
     <!-- 面包屑导航区 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/homedddddddDDD' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
@@ -47,7 +47,12 @@
                 @click="removeUserById(scope.row.id)"
               ></el-button>
               <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
-                <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+                <el-button
+                  type="warning"
+                  icon="el-icon-setting"
+                  size="mini"
+                  @click="setRole(scope.row)"
+                ></el-button>
               </el-tooltip>
             </template>
           </el-table-column>
@@ -103,6 +108,33 @@
       <span slot="footer" class="dialog-footer">
         <el-button @click="editDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="editUserInfo">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 分配角色的对话框 -->
+    <el-dialog
+      title="分配角色"
+      :visible.sync="setRoleDialogVisible"
+      width="50%"
+      @close="setRoleDialogClosed"
+    >
+      <div>
+        <p>当前的用户：{{userInfo.username}}</p>
+        <p>当前的角色：{{userInfo.role_name}}</p>
+        <p>
+          分配新角色：
+          <el-select v-model="selectedRoleId" placeholder="请选择">
+            <el-option
+              v-for="item in rolesList"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </p>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="setRoleDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveRoleInfo">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -175,7 +207,15 @@ export default {
           { required: true, message: '请输入手机', trigger: 'blur' },
           { validator: checkMobile, trigger: 'blur' }
         ]
-      }
+      },
+      //控制分配对话框的显示与隐藏
+      setRoleDialogVisible: false,
+      //需要被分配角色的用户信息
+      userInfo: '',
+      //所有角色的数据列表
+      rolesList: [],
+      //已选中的角色id值
+      selectedRoleId: []
     };
   },
   created() {
@@ -227,7 +267,9 @@ export default {
         if (!valid) return;
         //可以发起添加用户的请求
         const { data: res } = await this.$http.post('users', this.addForm);
-        if (res.meta.status != 201) this.$message.error('添加用户失败');
+        if (res.meta.status != 201) {
+          return this.$message.error('添加用户失败');
+        }
         this.$message.success('添加用户成功');
         this.addDialogVisible = false;
         this.getUserList();
@@ -241,6 +283,7 @@ export default {
         return this.$message.error('查询用户信息失败！');
       }
       this.editForm = res.data;
+
       this.editDialogVisible = true;
     },
     //监听修改用户对话框的关闭事件
@@ -289,12 +332,49 @@ export default {
       if (confirmResult !== 'confirm') {
         return this.$message.info('已取消删除');
       }
-      const {data:res} = await this.$http.delete('users/' + id)
+      const { data: res } = await this.$http.delete('users/' + id);
       if (res.meta.status !== 200) {
-          return this.$message.error('删除用户信息失败！');
+        return this.$message.error('删除用户信息失败！');
+      }
+      this.$message.success('删除用户信息成功！');
+      this.getUserList();
+    },
+    //展示分配角色的对话框
+    async setRole(userInfo) {
+      this.userInfo = userInfo;
+
+      //在展示对话框前，获取所有角色的列表
+      const { data: res } = await this.$http.get('roles');
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败！');
+      }
+      this.rolesList = res.data;
+
+      this.setRoleDialogVisible = true;
+    },
+    //点击按钮，分配角色
+    async saveRoleInfo() {
+      if (!this.selectedRoleId) {
+        return this.$message.error('请选择新角色');
+      }
+
+      const { data: res } = await this.$http.put(
+        `users/${this.userInfo.id}/role`,
+        {
+          rid: this.selectedRoleId
         }
-          this.$message.success('删除用户信息成功！')
-          this.getUserList();
+      );
+      if (res.meta.status !== 200) {
+        return this.$message.error('更新角色失败！');
+      }
+      this.$message.success('更新角色成功！');
+      this.getUserList();
+      this.setRoleDialogVisible = false;
+    },
+    //监听分配角色对话框的关闭事件
+    setRoleDialogClosed() {
+      this.selectedRoleId = '';
+      this.userInfo = '';
     }
   }
 };
--- src/router/router.js
+++ src/router/router.js
@@ -4,44 +4,56 @@ import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
+import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
 
 
 Vue.use(VueRouter)
 
-  const routes = [
-    { 
-      path:'/login',
-      component : Login
-    } ,
-    { 
-      path:'/',
-      redirect:'/login'
-    } ,
-    { 
-      path:'/home',
-      component : Home,
-      redirect:"/welcome",
-      children:[
-        {path: '/welcome',
-        component: Welcome},
-        {
-          path:'/users',
-          component:Users
-        }
-      ]
-    } 
-  
+const routes = [
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/home',
+    component: Home,
+    redirect: "/welcome",
+    children: [
+      {
+        path: '/welcome',
+        component: Welcome
+      },
+      {
+        path: '/users',
+        component: Users
+      },
+      {
+        path:'/rights',
+        component: Rights
+      },
+      {
+        path:'/roles',
+        component: Roles
+      }
+    ]
+  }
+
 ]
 
 const router = new VueRouter({
   routes
 })
 
-router.beforeEach( (to,from,next) => {
-  if ( to.path === '/login' ) return next();
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next();
 
   const tokenStr = window.sessionStorage.getItem('token')
-  if ( !tokenStr ) return next('/login')
+  if (!tokenStr) return next('/login')
 
   next()
 })
