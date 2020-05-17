c041157756e331ad5796459f6aa8ae51cb9058fd
svuv <1737748525@qq.com>
2020-05-17 14:36:33 +0800

    完成商品分类功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -6,7 +6,7 @@ import Vue from 'vue'
 
 import {
     Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem
-    , Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option
+    , Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader
 } from 'element-ui'
 
 Vue.use(Button)
@@ -36,5 +36,6 @@ Vue.use(Tag)
 Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Cascader)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
